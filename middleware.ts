7f69ed4d9bd678c9dd381fceb066dca5fff29b55
Axcio <contact@axcio-data.com>
2025-11-22 13:57:09 +0100

    modification middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -50,6 +50,6 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/(.*)'],
   runtime: 'nodejs',
 };
